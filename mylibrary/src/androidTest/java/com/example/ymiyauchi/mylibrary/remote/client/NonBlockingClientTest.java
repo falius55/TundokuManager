d4e9ee7fa7aa3cb36c8c0a731a201868aa9b3594
ymiyauchi <ciero639@gmail.com>
2017-03-15 03:49:22 +0900

    git管理対象ファイルの変更

---
+++ mylibrary/src/androidTest/java/com/example/ymiyauchi/mylibrary/remote/client/NonBlockingClientTest.java
@@ -0,0 +1,10 @@
+package com.example.ymiyauchi.mylibrary.remote.client;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ymiyauchi on 2017/03/02.
+ */
+public class NonBlockingClientTest {
+
+}
